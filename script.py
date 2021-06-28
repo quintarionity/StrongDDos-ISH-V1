52f4473c6dbff96172ea3c51b5d260a4d3f4c03e
L0G1K200 <75788512+L0G1K200@users.noreply.github.com>
2021-06-28 21:54:58 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,5 @@
 import sys, os, time, socket, random
 from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 os.system("clear")
