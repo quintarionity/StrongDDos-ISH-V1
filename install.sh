befb8ba6ab4e5a42d997b4f79d3404d6c0515ed6
L0G1K200 <75788512+L0G1K200@users.noreply.github.com>
2021-06-29 11:03:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,10 @@
-if [[ $EUID -ne 0 ]]; then
-   echo "Error, please run this script as root user!"
-   exit 1
-fi
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN="\e[32m"
-apt install figlet -y
+apk add figlet -y
 clear
 figlet "StrongDDos"
 echo "\n"
-echo "${RED}[*] Started installing StrongDDos script!"
-echo "${NC}\n"
-apt install python2 python3
+echo "[*] Started installing StrongDDos script!"
+echo "\n"
+apk add python2 python3
 clear
 figlet "All Packages Successfully installed"
-echo "${GREEN}[*] Now you can run script with command 'python2 script.py'"
+echo "[*] Now you can run script with command 'python2 script.py'"
